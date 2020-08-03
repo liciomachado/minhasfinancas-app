47ba7d9c62117cd0392c93bbe57d1f5336cf6f18
Mauricio Machado <licio.machado@hotmail.com>
2020-08-02 21:53:54 -0300

    alterações apos controle de login

---
+++ src/app/service/usuarioService.js
@@ -0,0 +1,49 @@
+import ApiService from '../apiservice'
+
+import ErroValidacao from '../exception/erroValidacao'
+
+class UsuarioService extends ApiService {
+
+    constructor(){
+        super('/api/usuarios')
+    }
+
+    autenticar(credenciais){
+        return this.post('/autenticar', credenciais)
+    }
+
+    obterSaldoPorIdUsuario(id) {
+        return this.get(`/${id}/saldo`)
+    }
+
+    salvar(usuario){
+        return this.post('/', usuario);
+    }
+
+    validar (usuario){
+        const erros = []
+
+        if(!usuario.nome){
+            erros.push('O campo Nome é obrigatório.')
+        }
+
+        if(!usuario.email){
+            erros.push('O campo Email é obrigatório.')
+        }else if(this.state.email.match(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]/)){
+            erros.push('Informe um email válido.')
+        }
+        
+        if(!usuario.senha || usuario.senhaRepeticao){
+            erros.push('Digite a senha 2x')
+        }else if(usuario.senha !== usuario.senhaRepeticao){
+            erros.push('As senhas digitadas não batem.')
+        }
+
+        if(erros && erros.length > 0){
+            throw new ErroValidacao(erros);
+        }
+        return erros;
+    }
+}
+
+export default UsuarioService;
